e4a0ef938387bdcea8bf9691471766aeb56c7d75
2100055 <162372360+2100055@users.noreply.github.com>
2025-02-24 14:43:26 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ class Converter():
         self.temp_frame =Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
+        self.temp_heading = Label(self.temp_frame,
+                            text="Temperature Convertor",
+                            font=("Arial", "16", "bold")
+                             )
+        self.temp_heading.grid(row=0)
+
+        instructions = ("Please enter a temperature below and then press "
+                         "one of the buttons to convert it from centigrade " 
+                         "to fahrenheit.")
+        self.temp_intstructions = Label(self.temp_frame,
+                                        text=instructions,
+                                        wraplength=250, width=40,
+                                        justify="left")
+        self.temp_intstructions.grid(row=1)
+        
 #main routine
 
 
