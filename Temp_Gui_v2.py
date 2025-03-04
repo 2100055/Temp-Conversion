722857d0e631c1302689b2f705ae2329c1bfed3b
2100055 <162372360+2100055@users.noreply.github.com>
2025-03-05 09:51:40 +1300

    Add files via upload

--- Temp_Gui_v2.py
+++ Temp_Gui_v2.py
@@ -48,7 +48,7 @@ class Converter():
             ["To Celsius", "#990099", "", 0, 0],
             ["To Fahrenheit", "#009900", "", 0, 1], 
             ["Help / Info", "#CC6600", "", 1, 0],
-            ["History / Export ", "004C99", "", 1, 1]
+            ["History / Export ", "#004C99", "", 1, 1]
         ]
 
         # List to hold buttons once they have been made
@@ -57,9 +57,14 @@ class Converter():
         for item in button_details_list:
             self.make_button = Button(self.button_frame,
                                       text=item[0], bg =item[1],
-                                      fg="FFFFFF", font=("Arial", "12" , "bold"),
+                                      fg="#FFFFFF", font=("Arial", "12" , "bold"),
                                       width=12, command=item[2])
             self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.button_ref_list.append(self.make_button)
+
+        # retrieve 'history / export button and disable it at the start
+        self.to_history_button=self.button_ref_list[3].config(state=DISABLED)
 #main routine
 
 
